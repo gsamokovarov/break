60197e990d40a3d0de94e710945b5d0c9e747d17
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-04-29 09:13:17 -0500

    Lower the Ruby requirement to 2.5
    People still use it and JRuby doesn't officially supports 2.6 ATM so try
    to target what Ruby version is still used in the moment.

--- lib/break/binding_ext.rb
+++ lib/break/binding_ext.rb
@@ -3,4 +3,8 @@ class Binding
     context = Break::Context.new(self)
     context.start
   end
+
+  def source_location
+    eval "[__FILE__, __LINE__.to_i]"
+  end unless method_defined?(:source_location)
 end
