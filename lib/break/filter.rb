ffc56185923447b57c33ff2e4a31e3ad5a652ea4
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-06-11 21:15:35 +0300

    Register internal filters instead of directly augmenting them

--- lib/break/filter.rb
+++ lib/break/filter.rb
@@ -4,14 +4,16 @@ module Break
   module Filter extend self
     attr_reader :internal
 
-    @internal = [
-      IRB.method(:start).source_location.first.chomp(".rb"),
-      "(irb)",
-      __dir__,
-    ]
+    def register_internal(*paths)
+      (@internal ||= []).concat(paths)
+    end
 
     def internal?(path)
       @internal.any? { |location| path.include?(location) }
     end
   end
+
+  Filter.register_internal IRB.method(:start).source_location.first.chomp(".rb")
+  Filter.register_internal "(irb)"
+  Filter.register_internal __dir__
 end
--- test/break/filter_test.rb
+++ test/break/filter_test.rb
@@ -6,6 +6,19 @@ module Break
       assert Filter.internal?("(irb)")
       assert Filter.internal?(binding.method(:break).source_location.first)
     end
+
+    test "registers new internal frames" do
+      register_internal "(pry)" do
+        assert Filter.internal?("(pry)")
+      end
+    end
+
+    def register_internal(*paths)
+      original_internal = [].concat(Filter.internal)
+      Filter.register_internal(*paths)
+      yield
+    ensure 
+      Filter.internal.clear.concat(original_internal)
+    end
   end
 end
-
