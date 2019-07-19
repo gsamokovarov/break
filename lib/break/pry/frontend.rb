7b9caa4c10aae075d345ae81809601c043d1f73e
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-07-19 16:20:27 +0300

    Drop the separate gems for integrations
    Try having it all in one monorepo and see how it works.

---
+++ lib/break/pry/frontend.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Break::Pry
+  class Frontend
+    def initialize(pry)
+      @pry = pry
+    end
+
+    def attach(session)
+      @pry = Pry.start(session.context.binding)
+      where
+    end
+
+    def detach
+      throw :breakout
+    end
+
+    def where
+      @pry&.run_command "whereami".dup
+    rescue MethodSource::SourceNotFoundError
+      puts "Cannot find method code."
+    end
+
+    def notify(message)
+      puts message
+    end
+  end
+end
