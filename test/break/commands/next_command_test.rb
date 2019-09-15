f26925263b47ad872383960ea6e08c2fb407de5b
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-09-15 15:13:58 +0300

    Test the next command

---
+++ test/break/commands/next_command_test.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Break
+  class NextCommandsTest < Test
+    test "increments the debugging context depth on :call and :class events" do
+      command, session = next_command
+
+      command.execute_trace trace(:call, binding: nil)
+      command.execute_trace trace(:class, binding: nil)
+
+      assert_equal 2, session.context.depth
+    end
+
+    test "decrements the debugging context depth on :return and :end events" do
+      command, session = next_command
+
+      command.execute_trace trace(:return, binding: nil)
+      command.execute_trace trace(:end, binding: nil)
+
+      assert_equal(-2, session.context.depth)
+    end
+
+    test "disables the current tracing on each step over" do
+      command, session = next_command
+
+      disabled = false
+      command.execute_trace trace(:line, binding: nil, disable: -> { disabled = true })
+
+      assert disabled
+    end
+
+    test "disables the current tracing on each step over" do
+      command, session = next_command
+
+      disabled = false
+      command.execute_trace trace(:line, binding: nil, disable: -> { disabled = true })
+
+      assert disabled
+    end
+
+    test "creates new debugging context on zero or negative depth" do
+      command, session = next_command
+
+      # This is a common case – step-into a one-line method and then step-over.
+      command.execute_trace trace(:return, binding: nil)
+      command.execute_trace trace(:line, disable: nil, binding: -> { Kernel.binding })
+
+      assert_equal __LINE__ - 2, session.context.binding.source_location.last
+    end
+
+    def next_command
+      session = Session.new(binding, frontend: TestingFrontend.new)
+
+      [NextCommand.new(session), session]
+    end
+
+    def trace(event, **methods)
+      trace = Object.new
+      trace.define_singleton_method(:event) { event }
+      methods.each do |name, implementation|
+        implementation = -> { implementation } unless implementation.respond_to?(:call)
+        trace.define_singleton_method(name, &implementation)
+      end
+      trace
+    end
+  end
+end
