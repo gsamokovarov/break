83286cc4133366f002ba7838aba9df47ec925b96
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-09-28 14:17:46 +0300

    Introduce fixtures to test against most common debuggers

---
+++ test/fixtures/byebug.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "byebug"
+
+byebug
+
+class Foo
+  x = 2
+  y = 3
+  x + y
+end
+
+def test_basic_call
+  result = 42
+  result += 1
+  byebug
+  4.times do
+    result = sum(result, 2)
+  end
+  result = sum(result, 2)
+  result += 1
+  result
+  result
+end
+
+def test_crashing_program
+  result = 40
+  byebug
+  resutt += 2
+  result
+end
+
+def sum(a, b)
+  a + b
+end
+
+puts test_basic_call
+begin
+  puts test_crashing_program
+rescue
+  nil
+end
+expression = :unused.to_s + "variable"
+puts "END OF RUN"
